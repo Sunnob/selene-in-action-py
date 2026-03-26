fbadc6dc3fa10c588958bc79777a2fcfea0863a5
Sunnob/CZU-block-study <hakimovsunat@gmail.com>
2026-03-26 22:49:33 +0100

    Timeout 2.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,9 @@ from selenium import webdriver
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://todomvc.com/examples/emberjs/todomvc/dist/'
+    browser.config.timeout = 2.0
     driver_options = webdriver.FirefoxOptions()
-    driver_options.add_argument('--headless')
+    driver_options.add_argument('--headless=new')
     browser.config.driver_options = driver_options
 
     yield
