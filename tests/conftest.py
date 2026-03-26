fe33c0f8ffcac502820a530e7cb2f34e1790eeef
Sunnob/CZU-block-study <hakimovsunat@gmail.com>
2026-03-26 22:21:34 +0100

    Added webdriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 
@@ -8,6 +9,7 @@ from selene import browser
 def browser_management():
     browser.config.base_url = 'https://todomvc.com/examples/emberjs/todomvc/dist/'
     browser.config.driver_name = 'firefox'
+    browser.config.driver_options = webdriver.FirefoxOptions()
 
     yield
 
