df5c650e6b149e6ef8bbae143da5ad658d9472f2
Sunnob/CZU-block-study <hakimovsunat@gmail.com>
2026-03-28 22:19:07 +0100

    Difference between selene and selenium

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selene import browser
 from selenium import webdriver
-
-
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -11,8 +11,30 @@ def browser_management():
     browser.config.timeout = 2.0
     driver_options = webdriver.FirefoxOptions()
     driver_options.add_argument('--headless=new')
-    browser.config.driver_options = driver_options
+    # browser.config.driver_options = driver_options
+    browser.config.driver = webdriver.Chrome(
+        service=ChromeService(executable_path=ChromeDriverManager().install()),
+        options=driver_options,
+    )
 
     yield
 
     browser.quit()  # closing browser
+
+@pytest.fixture()
+def driver():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless=new')
+    driver = webdriver.Chrome(
+        service=ChromeService(executable_path=ChromeDriverManager().install()),
+        options=driver_options,
+    )
+
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def browser(driver):
+
+    yield Browser(Config(driver=driver))
--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -1,5 +1,8 @@
-from selene import browser, have, be
+from selene import browser, have, be, by
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 def test_complete_todo():
     browser.open('/') #opening browser
@@ -10,3 +13,18 @@ def test_complete_todo():
     browser.element('.new-todo').type('b').press_enter()
     browser.element('.new-todo').type('c').press_enter()
     browser.all('.todo-list>li').with_(timeout=browser.config.timeout * 1.5).should(have.size(3))
+
+    browser.all('.todo-list>li').should(have.size(3))  # selene
+    browser.all('.todo-list>li').wai.for_(have.size(3))
+
+
+    '''
+    assert len(browser.driver.find_elements(*by.css('.todo-list>li'))) == 3
+    WebDriverWait(driver=browser.driver,timeout=3.0).until(lambda driver: len(driver.find_elements(*by.css('.todo-list>li'))) == 3) #selenium webdriver
+    '''
+
+def test_complete_todo(driver):
+
+   driver.get('https://todomvc.com/examples/emberjs/todomvc/dist/')
+
+   driver.find_element(*by.css('.new-todo')).send_keys('a' + Keys.ENTER)
